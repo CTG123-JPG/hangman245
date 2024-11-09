4b4bf8c2aae9eb7e58456df9d7a2fdeec7e94d8c
CTG123-JPG <evinkuruvilla@gmail.com>
2024-11-09 20:48:44 +0000

    Update milestone2.py

--- hangman/milestone2.py
+++ hangman/milestone2.py
@@ -5,5 +5,8 @@ print(random.choice(word_list))
 
 guess = input(f'Input a Letter')
 # Asks the user to input a letter
+if len(guess) == 1 and guess.isalpha():
+  print(f"Good Guess")
+else:
+   print(f"Please try again")
 
-print(guess)
