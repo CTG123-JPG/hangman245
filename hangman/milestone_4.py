3bf262417512f6e0b3a011a08562bbb9e0a08b38
CTG123-JPG <evinkuruvilla@gmail.com>
2024-11-11 00:06:31 +0000

    Update milestone_4.py

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -4,10 +4,10 @@ class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = random.choice(word_list)  # select a random word from the list
-        self.word_guessed = ['_'] * len(self.word)  # initialize word_guessed with underscores
-        self.num_letters = len(self.word)  # initialize num_letters to the length of the word
-        self.list_of_guesses = []  # initialize list_of_guesses as an empty list
+        self.word = random.choice(word_list)  
+        self.word_guessed = ['_'] * len(self.word) 
+        self.num_letters = len(self.word) 
+        self.list_of_guesses = [] 
 
     def ask_for_input(self):
         while True:
@@ -23,19 +23,18 @@ class Hangman:
     def check_guess(self, guess):
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            for i in range(len(self.word)):
-                if self.word[i] == guess:
-                    self.word_guessed[i] = guess
+            for num in range(len(self.word)):
+                if self.word[num] == guess:
+                    self.word_guessed[num] = guess
             self.num_letters -= 1
         else:
             print(f"Sorry, {guess} is not in the word. Try again.")
             self.num_lives -= 1
         print(f"You have {self.num_lives} lives remaining.")
-        print(' '.join(self.word_guessed))  # print the current state of the word
+        print(' '.join(self.word_guessed))
 
     def play_game(self):
         self.ask_for_input()
 
-# create an instance of the class
 game = Hangman(['apple', 'banana', 'cherry'], 5)
 game.play_game()
