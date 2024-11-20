ee1f7076072f66c391be1faa0f7e39bd43e9a566
CTG123-JPG <evinkuruvilla@gmail.com>
2024-11-20 22:42:22 +0000

    Update milestone2.py

--- hangman/milestone2.py
+++ hangman/milestone2.py
@@ -1,12 +1,18 @@
 import random
-word_list = ["Dragon Fruit", "Apple", "Grape", "Orange","Pinapple"]
+word_list = ["Dragon Fruit", "Apple", "Grape", "Orange","Pineapple"] # A list of words in string format containing the names of fruits needed for the rest of the code to run
 
-print(random.choice(word_list))
 
-guess = input(f'Input a Letter')
-# Asks the user to input a letter
+word = random.choice(word_list) # Allocates a random word from the word list to the variable names "word"
+
+
+print(word) # Prints the content in the variable "word"
+
+
+guess = input(f'Input a Letter') # Asks the user to input a letter
+
+
 if len(guess) == 1 and guess.isalpha():
   print(f"Good Guess")
 else:
-   print(f"Please try again")
+   print(f"Oops! That is not a valid input")
 
