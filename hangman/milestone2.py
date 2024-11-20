c10650c22894695da3ead9c0ebd9ee97c885a96b
CTG123-JPG <evinkuruvilla@gmail.com>
2024-11-20 23:12:58 +0000

    Update milestone2.py

--- hangman/milestone2.py
+++ hangman/milestone2.py
@@ -11,8 +11,7 @@ print(word) # Prints the content in the variable "word"
 guess = input(f'Input a Letter') # Asks the user to input a letter
 
 
-if len(guess) == 1 and guess.isalpha():
-  print(f"Good Guess")
+if len(guess) == 1 and guess.isalpha(): # Checks the number of characters in the input from the user and checkks if it is equal to 1
+  print(f"Good Guess") # If the number of characters is 1 the code continues with "Good Guess"
 else:
-   print(f"Oops! That is not a valid input")
-
+   print(f"Oops! That is not a valid input") # If the user inputted more than 1 character then the code displays "Oops! That is not a valid input"
