75fede0186b401ce4483f7a782161b80d252fe33
CTG123-JPG <evinkuruvilla@gmail.com>
2024-11-21 21:19:48 +0000

    Create word_list_for_hangman.py

---
+++ hangman/word_list_for_hangman.py
@@ -0,0 +1,5 @@
+import random
+word_list = ["Dragon Fruit", "Apple", "Grape", "Orange","Pineapple"] # A list of words in string format containing the names of fruits needed for the rest of the code to run
+
+
+word = random.choice(word_list) # Allocates a random word from the word list to the variable names "word"
